694f1e474978e09d4786d96044dbd39b7057ea28
Rehan1125 <145065520+Rehan1125@users.noreply.github.com>
2023-09-24 23:17:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gk5iws
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gk5iws)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import NavBar from './Components/NavBar';
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
